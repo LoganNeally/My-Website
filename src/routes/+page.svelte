1f9d6b4d5f17f2104fb3b044949f9e9cf4760f70
LoganNeally <loganneally@gmail.com>
2025-02-12 20:26:56 -0500

    built website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h1 class="h1">Welcome to loganneallyit.net.</h1>
+		<h2 class="h2 card p-2">Welcome to my website</h2>
+		
+	</div>
+</div>
