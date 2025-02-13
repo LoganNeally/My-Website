39a86892b8656a33d15cbde9ad45f4cac0cd883c
LoganNeally <loganneally@gmail.com>
2025-02-12 22:05:57 -0500

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-20 text-center flex flex-col items-center">
-		<h1 style="color:chocolate;" class="h1">Welcome to loganneallyit.net.</h1>
+		<h1 style="color:chocolate;" class="h1">Welcome to loganneallyit.net</h1>
 		<h2 class="h2">Welcome to my website</h2>
 	</div>
 </div>
