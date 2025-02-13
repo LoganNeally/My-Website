8c489c588a8905f2ae979a4ea58e80dbe6253e82
LoganNeally <loganneally@gmail.com>
2025-02-12 20:37:25 -0500

    Uh oh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,6 @@
 	<div class="space-y-10 text-center flex flex-col items-center">
 		<h1 class="h1">Welcome to loganneallyit.net.</h1>
 		<h2 class="h2 card p-2">Welcome to my website</h2>
-		
+		<h2>Howdy</h2>
 	</div>
 </div>
