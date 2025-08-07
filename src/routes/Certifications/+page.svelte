66a7104ba60c49ab8ab981eaf9c09ab8f7761cee
LoganNeally <loganneally@gmail.com>
2025-08-07 19:03:58 -0400

    Added some stuff

--- src/routes/Certifications/+page.svelte
+++ src/routes/Certifications/+page.svelte
@@ -3,4 +3,71 @@
 		<h2 class="h2 flex flex-auto">Certifications</h2>
 		<hr class="!border-t-2 opacity-50" />
 	</div>
+
+	<div class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-4">
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Google Cybersecurity Professional Certificate</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						In Progress.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					<a class="btn btn-sm variant-soft-primary" href="https://www.coursera.org/professional-certificates/google-cybersecurity">Google Cybersecurity</a>
+					<small>2025</small>
+				</div>
+			</footer>
+		</div>
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Comptia Security+</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						In Progress.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					<a class="btn btn-sm variant-soft-primary" href="https://www.comptia.org/en-us/certifications/security/"
+						>Comptia Security+</a
+					>
+					<small>2025</small>
+				</div>
+			</footer>
+		</div>
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Comptia Network+</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						In Progress.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					<a class="btn btn-sm variant-soft-primary" href="https://www.comptia.org/en-us/certifications/network/"
+						>Comptia Network+</a
+					>
+					<small>2025</small>
+				</div>
+			</footer>
+		</div>
+	</div>
 </div>
--- src/routes/Education/+page.svelte
+++ src/routes/Education/+page.svelte
@@ -3,4 +3,46 @@
 		<h2 class="h2 flex flex-auto">Education</h2>
 		<hr class="!border-t-2 opacity-50" />
 	</div>
+	<div class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-4">
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Bell Creek Academy</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						High School
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					<small>2017-2021</small>
+				</div>
+			</footer>
+		</div>
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Hillsborough Community College</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						Associates in Arts for Cybersecurity
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					<small>2021-2025</small>
+				</div>
+			</footer>
+		</div>
+	</div>
+
+
 </div>
