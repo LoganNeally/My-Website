a07326ffec5f62480630812cd54810b871263edf
LoganNeally <loganneally@gmail.com>
2025-08-08 19:28:47 -0400

    Work exp and Project

--- src/routes/Work/+page.svelte
+++ src/routes/Work/+page.svelte
@@ -3,4 +3,54 @@
 		<h2 class="h2 flex flex-auto">Work Experience</h2>
 		<hr class="!border-t-2 opacity-50" />
 	</div>
+	<div class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-4">
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Winn Dixie, Riverview FL — Cashier</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						<li>•Work to cash out customers at my local Winn Dixie supermarket.</li>
+						<li>•Provide customers with an enjoyable and pleasant shopping experience.</li>
+						<li>•Make sure I give the best customer service I possibly can.</li>
+
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					
+					<small>March 2020 - May 2021</small>
+				</div>
+			</footer>
+		</div>
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Jimmy Johns, Brandon FL — Assistant Manager</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						<li>•Handle cash in the registers and count the store’s safe.</li>
+						<li>•Provide customers with a welcoming environment and excellent service</li>
+						<li>•Ability to safely handle food and other products.</li>
+						<li>•Manage staff and labor during my shift.</li>
+						<li>•Ability to resolve issues regarding customers or employees while on my shift. 
+							</li>
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					
+					<small>June 2021 - Present</small>
+				</div>
+			</footer>
+		</div>
+	</div>
 </div>
