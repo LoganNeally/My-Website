8ae3e716a729e8c34fa63119df2d8c8547caf8cb
LoganNeally <loganneally@gmail.com>
2026-01-06 14:21:10 -0500

    small mod

--- src/routes/Certifications/+page.svelte
+++ src/routes/Certifications/+page.svelte
@@ -20,7 +20,7 @@
 			<hr class="!border-t-2 opacity-50" />
 			<footer class="p-4 flex justify-start items-center space-x-4">
 				<div class="flex-auto flex justify-between items-center">
-					<a class="btn btn-sm variant-soft-primary" href="https://www.coursera.org/professional-certificates/google-cybersecurity">Google Cybersecurity</a>
+					<a class="btn btn-sm variant-soft-primary" href=" ">Akamai Network Engineering Certificate</a>
 					<small>2026</small>
 				</div>
 			</footer>
