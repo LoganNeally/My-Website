bcc23e2692b37516ecf64f0c6c3fa5491a2238d0
LoganNeally <loganneally@gmail.com>
2026-01-06 14:23:54 -0500

    small

--- src/routes/Work/+page.svelte
+++ src/routes/Work/+page.svelte
@@ -3,30 +3,9 @@
 		<h2 class="h2 flex flex-auto">Work Experience</h2>
 		<hr class="!border-t-2 opacity-50" />
 	</div>
+	
 	<div class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-4">
-		<div class="block shadow-md card p-4">
-			<header></header>
-			<div class="p-4 space-y-4">
-				<h3 class="h3" data-toc-ignore>Winn Dixie, Riverview FL — Cashier</h3>
-				<article>
-					<p>
-						<!-- cspell:disable -->
-						<li>•Work to cash out customers at my local Winn Dixie supermarket.</li>
-						<li>•Provide customers with an enjoyable and pleasant shopping experience.</li>
-						<li>•Make sure I give the best customer service I possibly can.</li>
 
-						<!-- cspell:enable -->
-					</p>
-				</article>
-			</div>
-			<hr class="!border-t-2 opacity-50" />
-			<footer class="p-4 flex justify-start items-center space-x-4">
-				<div class="flex-auto flex justify-between items-center">
-					
-					<small>March 2020 - May 2021</small>
-				</div>
-			</footer>
-		</div>
 		<div class="block shadow-md card p-4">
 			<header></header>
 			<div class="p-4 space-y-4">
@@ -52,5 +31,31 @@
 				</div>
 			</footer>
 		</div>
+
+
+		<div class="block shadow-md card p-4">
+			<header></header>
+			<div class="p-4 space-y-4">
+				<h3 class="h3" data-toc-ignore>Winn Dixie, Riverview FL — Cashier</h3>
+				<article>
+					<p>
+						<!-- cspell:disable -->
+						<li>•Work to cash out customers at my local Winn Dixie supermarket.</li>
+						<li>•Provide customers with an enjoyable and pleasant shopping experience.</li>
+						<li>•Make sure I give the best customer service I possibly can.</li>
+
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="!border-t-2 opacity-50" />
+			<footer class="p-4 flex justify-start items-center space-x-4">
+				<div class="flex-auto flex justify-between items-center">
+					
+					<small>March 2020 - May 2021</small>
+				</div>
+			</footer>
+		</div>
+		
 	</div>
 </div>
